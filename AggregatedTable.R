6de9e3133c61084711ca4193a3209d40f6049a8e
Arnav Mohan <165528328+arnmoh18@users.noreply.github.com>
2024-05-17 21:56:29 -0700

    Update AggregatedTable.R

--- AggregatedTable.R
+++ AggregatedTable.R
@@ -1,10 +1,13 @@
 # Assuming your data frame is named 'Data'
 library(dplyr)
+file_path <- "https://raw.githubusercontent.com/info-201b-sp24/exploratory-analysis-arnmoh18/main/Data.csv"
 
+# Load the CSV file
+Data <- read.csv(file_path)
 # Create a new table that summarizes the maximum loss percentage by country and commodity
 max_loss_by_country_commodity <- Data %>%
   group_by(country, commodity) %>%
-  summarize(max_loss = max(as.numeric(gsub("%", "", loss_percentage)), na.rm = TRUE)) %>%
+  summarize(max_loss = round(max(as.numeric(gsub("%", "", loss_percentage)), na.rm = TRUE), 2)) %>%
   arrange(desc(max_loss))
 
 # View the resulting table
